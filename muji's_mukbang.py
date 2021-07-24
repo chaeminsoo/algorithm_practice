e41ab3465a9092b67748ab05e96da7bcf18aa3c1
chaeminsoo <chaems97@gmail.com>
2021-07-24 17:28:16 +0900

    Greedy

--- muji's_mukbang.py
+++ muji's_mukbang.py
@@ -1,19 +1,35 @@
+import heapq
+
 def solution(food_times, k):
-    answer = 0
-    cnt = 0
     
+    if sum(food_times) <=k:
+        return -1
     
-    for index in range(len(food_times)):
+    foods=[]
+    leng = len(food_times)
         
-        if food_times[index] ==0:
-            continue
+    for i in range(leng):
+        heapq.heappush(foods,[food_times[i],i+1])
+    
+    ref_num=0
+    ref_k = k
+    while foods:
+        ref_num+=foods[0][0]
+        ref_k -= foods[0][0]*len(foods)
+        
+        if ref_k<=0:
+            break
         else:
-            food_times[index] = food_times[index] -1
-            cnt +=1
-            
-                       
+            heapq.heappop(foods)
+            foods[0][0] -= ref_num
+            k = ref_k
+
+    address=k%len(foods)
+    
+    foods.sort(key=lambda x:x[1])
+    
+    
+    answer = foods[address][1]
+          
     
     return answer
-
-if __name__ == '__main__':
-    solution([3,1,2],5)
