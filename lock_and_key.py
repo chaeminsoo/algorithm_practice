1332ab568cea7a9ca5bca34d607c0ff816a8711b
chaeminsoo <chaems97@gmail.com>
2021-08-03 01:08:45 +0900

    75%_complete

--- lock_and_key.py
+++ lock_and_key.py
@@ -1,48 +1,61 @@
-import copy
-
 def solution(key, lock):
-    answer = True
+    answer = False
     
-    n=len(lock)
-    m=len(key)
+    m =len(key)
+    n = len(lock)
+    offset =  m-1
+    new_len = offset*2 +n
+    moving = offset+n
     
-    new_lock = [[0]*(2*m+(n-2)) for _ in range(2*m+(n-2))]
+    new_lock = [[0]*new_len for _ in range(new_len)]
+    
     
     for i in range(n):
         for j in range(n):
-            new_lock[(m-1)+i][(m-1)+j] = lock[i][j]
+            new_lock[i+offset][j+offset] = lock[i][j] 
     
-    ref_lock = copy.deepcopy(new_lock)
-    
-    area = n+(m-1)
-    
-    for i in range(area):
-        for j in range(area):
-            
-            
-    
-        
+    for i in range(moving):
+        for j in range(moving):
+            for rrot in range(4):
+                ref_key = rot_90(key)
+                
+                for k in range(m):
+                    for l in range(m):
+                        new_lock[k+i][l+j] += ref_key[k][l]
+
+                if check(new_lock, offset,n) == True:
+                    answer = True
+                    break
+                else:
+                    for k in range(m):
+                        for l in range(m):
+                            new_lock[k+i][l+j] -= key[k][l]
                     
+                    key = ref_key
+                    
+        if answer == True:
+            break
+    
     return answer
 
-def turn_90(a):
-    n=len(a)
-    m=len(a[0])
-    result = [[0]*n for _ in range(m)]
-    for i in range(n):
-        for j in range(m):
-            result[j][n-i-1] = a[i][j]
-    return result
-
-def check(a,k):
+def check(new_lock,offset,n):
     result = True
-    n=len(a)
-    m=len(k)
-    for i in range(m-1,m-1+n,1):
-        for j in range(m-1,m-1+n,1):
-            if a[i][j] != 1:
+    for i in range(n):
+        for j in range(n):
+            if new_lock[i+offset][j+offset] != 1:
                 result = False
                 break
         if result == False:
             break
     return result
+
+def rot_90(a):
+    n=len(a)
+    m=len(a[0])
+    result = [[0]*n for _ in range(m)]
+    for i in range(n):
+        for j in range(m):
+            result[j][n-i-1] = a[i][j]            
+    return result
+    
+print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
