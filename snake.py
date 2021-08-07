74d6e4280ee31d1b27ac202909ee9ef722f55c4b
chaeminsoo <chaems97@gmail.com>
2021-08-07 16:26:29 +0900

    Incomplete

--- snake.py
+++ snake.py
@@ -1,3 +1,10 @@
+def eat_apple(location,apples):
+    if location[0] in apples:
+        apples.remove(location[0])
+        return 1
+    else:
+        return 0
+    
 def trun(ddd,ooo):
     if ddd == 'u' and ooo == 'L':
         ddd = 'l'
@@ -24,7 +31,7 @@ k = int(input())
 apples=[]
 for i in range(k):
     row, column = map(int,input().split())
-    apples.append((row,column))
+    apples.append((row-1,column-1))
 
 l = int(input())
 moves=[]
@@ -48,26 +55,38 @@ while location[0][0] >=0 and location[0][0] < n and location[0][1] >=0 and locat
     if direction == 'u':
         cnt+=1
         location.insert(0,(location[0][0]-1,location[0][1]))
+        eaten_apples += eat_apple(location,apples)
+        
     elif direction == 'r':
         cnt+=1
         location.insert(0,(location[0][0],location[0][1]+1))
+        eaten_apples += eat_apple(location,apples)
+        
     elif direction == 'd':
         cnt+=1
         location.insert(0,(location[0][0]+1,location[0][1]))
+        eaten_apples += eat_apple(location,apples)
+        
     elif direction == 'l':
         cnt+=1
         location.insert(0,(location[0][0],location[0][1]-1))
+        eaten_apples += eat_apple(location,apples)
+        
+    #location = location[:eaten_apples]
     
-    if location[0] in apples:
-        apples.remove(location[0])
-        eaten_apples+=1
+    # if location[0] in apples:
+    #     apples.remove(location[0])
+    #     eaten_apples+=1
 
     # if len(location) != eaten_apples:
-    #     location.pop()
-
+    
 
     if location.count(location[0]) != 1:
         break
 
+print(cnt)
+print(apples)
 print(eaten_apples)
+print(location)
+print(len(location))
 # 2
