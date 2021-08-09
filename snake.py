2231f862cd391dbe33445ca2d4ab50ef59a3421f
chaeminsoo <chaems97@gmail.com>
2021-08-09 16:14:34 +0900

    Implementation

--- snake.py
+++ snake.py
@@ -1,10 +1,3 @@
-def eat_apple(location,apples):
-    if location[0] in apples:
-        apples.remove(location[0])
-        return 1
-    else:
-        return 0
-    
 def trun(ddd,ooo):
     if ddd == 'u' and ooo == 'L':
         ddd = 'l'
@@ -25,7 +18,6 @@ def trun(ddd,ooo):
     return ddd
 
 n = int(input())
-field = [[0]*n for _ in range(n)]
 
 k = int(input())
 apples=[]
@@ -55,38 +47,31 @@ while location[0][0] >=0 and location[0][0] < n and location[0][1] >=0 and locat
     if direction == 'u':
         cnt+=1
         location.insert(0,(location[0][0]-1,location[0][1]))
-        eaten_apples += eat_apple(location,apples)
         
     elif direction == 'r':
         cnt+=1
         location.insert(0,(location[0][0],location[0][1]+1))
-        eaten_apples += eat_apple(location,apples)
         
     elif direction == 'd':
         cnt+=1
         location.insert(0,(location[0][0]+1,location[0][1]))
-        eaten_apples += eat_apple(location,apples)
         
     elif direction == 'l':
         cnt+=1
         location.insert(0,(location[0][0],location[0][1]-1))
-        eaten_apples += eat_apple(location,apples)
-        
-    #location = location[:eaten_apples]
     
-    # if location[0] in apples:
-    #     apples.remove(location[0])
-    #     eaten_apples+=1
-
-    # if len(location) != eaten_apples:
-    
+    if location[0] in apples:
+        apples.remove(location[0])
+        eaten_apples+=1
 
     if location.count(location[0]) != 1:
         break
+    
+    if len(location) != eaten_apples:
+        location.pop()
 
 print(cnt)
-print(apples)
-print(eaten_apples)
-print(location)
-print(len(location))
-# 2
+# print(apples)
+# print(eaten_apples)
+# print(location)
+# print(len(location))
