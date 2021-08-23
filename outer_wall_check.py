09e6eb38d4844244493847b11d8b0ba212dd086b
chaeminsoo <chaems97@gmail.com>
2021-08-23 20:54:03 +0900

    Incomplete

--- outer_wall_check.py
+++ outer_wall_check.py
@@ -1,45 +1,55 @@
-def check(dist,weak,all_weak):
-    long_d = dist.pop()
-    ref_list=[]
+def check(weak,dist,new_weak,n,cnt):
+    chosen = []
     for wp in weak:
-        chosen_wp =[]
-        cw = wp + long_d
-        ccw = wp - long_d
-        cw_list=[]
-        ccw_list=[]
-        for i in range(wp,cw+1):
-            if i in all_weak:
-                cw_list.append(i)
-        for i in range(ccw,wp+1):
-            if i in all_weak:
-                ccw_list.append(i)
-        
-        if len(cw_list) >= len(ccw_list):
-            chosen_wp = cw_list
-        else:
-            chosen_wp = ccw_list
-        chosen_wp.sort()
-        if len(chosen_wp) > len(ref_list):
-            ref_list = chosen_wp
-        elif len(chosen_wp) == len(ref_list):
-            rf_f, rf_l = ref_list[0],ref_list[-1]
-            c_f,c_l = chosen_wp[0], chosen_wp[-1]
-            if (rf_l - rf_f) >= (c_l - c_f):
-                pass
-            else:
-                ref_list = chosen_wp
-
-
+        friend = dist[-1]
+        covered = []
+        for i in range(wp,wp+friend+1):
+            if i in new_weak:
+                covered.append(i)
+        if len(chosen) < len(covered):
+            chosen = covered
+    # chosen is confirmed
+    for i in chosen:
+        if i >= n:
+            i-=n
+        if i in weak:
+            weak.remove(i)
+    dist.pop()
+    cnt+=1
+    
 
 def solution(n, weak, dist):
     answer = 0
     dist.sort()
-    past_weak = []
-    next_weak=[]
+    new_weak=[]
     for i in weak:
-        past_weak.append(i-n)
-        next_weak.append(i+n)
-    all_weak = past_weak + weak + next_weak
-    
-    
+        new_weak.append(i)
+        new_weak.append(i+n)
+    new_weak.sort()
+    # #----------------------------------------------------------
+    # cnt=0
+    # chosen = []
+    # for wp in weak:
+    #     friend = dist[-1]
+    #     covered = []
+    #     for i in range(wp,wp+friend+1):
+    #         if i in new_weak:
+    #             covered.append(i)
+    #     if len(chosen) < len(covered):
+    #         chosen = covered
+    # # chosen is confirmed
+    # for i in chosen:
+    #     if i >= n:
+    #         i-=n
+    #     weak.remove(i)
+    # cnt+=1
+    # # one down ---------------
+    cnt = 0
+    while weak:
+        try:
+            check(weak,dist,new_weak,n,cnt)
+        except IndexError:
+            answer = -1
+            break
+    answer = cnt        
     return answer
