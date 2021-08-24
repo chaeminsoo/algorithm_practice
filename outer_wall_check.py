e50d8ca040fd22212b55a465f791f89ba85ab71b
chaeminsoo <chaems97@gmail.com>
2021-08-24 21:57:16 +0900

    Incomplete

--- outer_wall_check.py
+++ outer_wall_check.py
@@ -1,27 +1,51 @@
-def check(weak,dist,new_weak,n,cnt):
+def check(weak,dist,new_weak,n,a):
     chosen = []
     for wp in weak:
-        friend = dist[-1]
+        friend = dist[a]#-1]
         covered = []
         for i in range(wp,wp+friend+1):
             if i in new_weak:
                 covered.append(i)
         if len(chosen) < len(covered):
             chosen = covered
+        # elif len(chosen) == len(covered):
+            # ref_cho , ref_cov = weak,weak
+            # for i in chosen:
+            #     if i >= n:
+            #         i-=n
+            #     if i in weak:
+            #         ref_cho.remove(i)
+            # for i in covered:
+            #     if i >= n:
+            #         i-=n
+            #     if i in weak:
+            #         ref_cov.remove(i)
+
+            # aaa = solution(n, chosen, dist)
+            # bbb = solution(n, covered, dist)
+            
+            # if aaa >= bbb:
+            #     chosen = covered
+            # else:
+            #     pass
+
     # chosen is confirmed
     for i in chosen:
         if i >= n:
             i-=n
         if i in weak:
             weak.remove(i)
-    dist.pop()
-    cnt+=1
+    #dist.pop()
+    a-=1
+    # #--
+    return 1
     
 
 def solution(n, weak, dist):
     answer = 0
     dist.sort()
     new_weak=[]
+    a=-1
     for i in weak:
         new_weak.append(i)
         new_weak.append(i+n)
@@ -44,13 +68,14 @@ def solution(n, weak, dist):
     #     weak.remove(i)
     # cnt+=1
     # # one down ---------------
-    cnt = 0
     while weak:
         try:
-            check(weak,dist,new_weak,n,cnt)
+            answer += check(weak,dist,new_weak,n,a)
         except IndexError:
             answer = -1
             break
-    answer = cnt        
     return answer
-    #github_test_2nd
+    
+
+print(solution(12,[1, 5, 6, 10],[1, 2, 3, 4]))
+print(solution(12,[1, 3, 4, 9, 10],[3, 5, 7]))
