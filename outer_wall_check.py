ac504a758127ba0731b4e593046c1d50a3c0ff13
chaeminsoo <chaems97@gmail.com>
2021-08-25 23:12:41 +0900

    Incomplete

--- outer_wall_check.py
+++ outer_wall_check.py
@@ -1,81 +1,98 @@
-def check(weak,dist,new_weak,n,a):
-    chosen = []
-    for wp in weak:
-        friend = dist[a]#-1]
-        covered = []
-        for i in range(wp,wp+friend+1):
-            if i in new_weak:
-                covered.append(i)
-        if len(chosen) < len(covered):
-            chosen = covered
-        # elif len(chosen) == len(covered):
-            # ref_cho , ref_cov = weak,weak
-            # for i in chosen:
-            #     if i >= n:
-            #         i-=n
-            #     if i in weak:
-            #         ref_cho.remove(i)
-            # for i in covered:
-            #     if i >= n:
-            #         i-=n
-            #     if i in weak:
-            #         ref_cov.remove(i)
-
-            # aaa = solution(n, chosen, dist)
-            # bbb = solution(n, covered, dist)
-            
-            # if aaa >= bbb:
-            #     chosen = covered
-            # else:
-            #     pass
-
-    # chosen is confirmed
-    for i in chosen:
-        if i >= n:
-            i-=n
-        if i in weak:
-            weak.remove(i)
-    #dist.pop()
-    a-=1
-    # #--
-    return 1
+# # 1st try
+# def check(weak,dist,new_weak,n,a):
+#     chosen = []
+#     for wp in weak:
+#         friend = dist[a]
+#         covered = []
+#         for i in range(wp,wp+friend+1):
+#             if i in new_weak:
+#                 covered.append(i)
+#         if len(chosen) < len(covered):
+#             chosen = covered
     
+#     for i in chosen:
+#         if i >= n:
+#             i-=n
+#         if i in weak:
+#             weak.remove(i)
+#     a-=1
+#     return 1
+    
+
+# def solution(n, weak, dist):
+#     answer = 0
+#     dist.sort()
+#     new_weak=[]
+#     a=-1
+#     for i in weak:
+#         new_weak.append(i)
+#         new_weak.append(i+n)
+#     new_weak.sort()
+#     while weak:
+#         try:
+#             answer += check(weak,dist,new_weak,n,a)
+#         except IndexError:
+#             answer = -1
+#             break
+#     return answer
+    
+# print(solution(12,[1, 5, 6, 10],[1, 2, 3, 4])) # 2
+# print(solution(12,[1, 3, 4, 9, 10],[3, 5, 7])) # 1
+#-----------------------------------------------------------------------------------------------------------------------------------------
+from itertools import permutations
+
+def close_num(targrt,liist):
+    ref = []
+    for i in range(len(liist)):
+        val = liist[i]
+        distance = abs(targrt-val)
+        ref.append((distance,i))
+    ref.sort()
+    return liist[ref[0][1]]
 
 def solution(n, weak, dist):
     answer = 0
-    dist.sort()
-    new_weak=[]
-    a=-1
+    new_weak = []
     for i in weak:
         new_weak.append(i)
         new_weak.append(i+n)
     new_weak.sort()
-    # #----------------------------------------------------------
-    # cnt=0
-    # chosen = []
-    # for wp in weak:
-    #     friend = dist[-1]
-    #     covered = []
-    #     for i in range(wp,wp+friend+1):
-    #         if i in new_weak:
-    #             covered.append(i)
-    #     if len(chosen) < len(covered):
-    #         chosen = covered
-    # # chosen is confirmed
-    # for i in chosen:
-    #     if i >= n:
-    #         i-=n
-    #     weak.remove(i)
-    # cnt+=1
-    # # one down ---------------
-    while weak:
-        try:
-            answer += check(weak,dist,new_weak,n,a)
-        except IndexError:
-            answer = -1
-            break
-    return answer
-    
 
-print(solution(12,[1, 5, 6, 10],[1, 2, 3, 4]))
-print(solution(12,[1, 3, 4, 9, 10],[3, 5, 7]))
+    Xs = permutations(dist,len(dist))
+    min_cnt = float('INF')
+    for sp in weak:
+        for x in Xs:
+            cnt = 1
+            # f_cnt = 0
+            check =[]
+            for j in range(len(x)):#while cnt < len(dist):
+                friend = x[j]
+                # try:
+                #     friend = x[f_cnt]
+                # except IndexError:
+                #     break
+
+                for k in range(sp,sp+friend+1):
+                    if k in new_weak:
+                        if k >= n: k -= n
+                        check.append(k)
+                check = list(set(check))
+                # cnt = j
+                # # f_cnt+=1
+                check.sort()
+                if check == weak:
+                    break
+                else:
+                    cnt +=1
+                    sp = close_num(sp+friend,weak)
+                min_cnt = min(min_cnt,cnt)
+    
+    # if min_cnt > len(dist):
+    #     answer = -1
+    # else:
+    answer = min_cnt
+
+    return answer
+
+print(solution(12,[1, 5, 6, 10],[1, 2, 3, 4])) # 2
+print(solution(12,[1, 3, 4, 9, 10],[3, 5, 7])) # 1
