4658d55c2cb8acb72b64f814aec9d0a5e22a9079
chaeminsoo <chaems97@gmail.com>
2021-08-27 16:25:24 +0900

    Incomplete

--- outer_wall_check.py
+++ outer_wall_check.py
@@ -1,48 +1,50 @@
 from itertools import permutations
 
-def close_num(targrt,liist):
-    ref = []
-    index=0
-    for i in range(len(liist)):
-        val = liist[i]
-        distance = val-targrt
-        ref.append((distance,i))
-    ref.sort()
-    for i in ref:
-        if i[0] > 0:
-            index = i[1]
+def close_num(target,liist):
+    ref = 0
+    for i in liist:
+        if i >= target:
+            ref = i
             break
-        else:
-            continue
-    return liist[index]
+    return ref
 
 def solution(n, weak, dist):
-    answer = 0
     wl = len(weak)
     new_weak = weak + [i + n for i in weak]
-    min_cnt = float('INF')
+    min_cnt = 100#float('inf')
     all_cases = list(permutations(dist,len(dist)))
     for sp in weak:
         for case in all_cases:
+            wp = sp
             cnt = 0
             weak_cnt = 0
             for friend in case:
-                arrived = sp +friend
+                arrived = wp +friend
 
                 for i in new_weak:
-                    if i >= sp and i <= arrived:
+                    if i >= wp and i <= arrived:
                         weak_cnt +=1
                 cnt += 1
                 if weak_cnt >= wl:
                     min_cnt = min(min_cnt,cnt)
                     break
                 else:
-                    sp  = close_num(sp+friend,weak)
-
-    answer = cnt        
-
-
+                    wp  = close_num(arrived,weak)
+                    # if wp == 0:
+                    #     break
+                    # # print(wp)##
+    
+    if min_cnt > 8:#== float('inf'):
+        answer = -1
+    else:
+        answer = min_cnt
+        
     return answer
+#-------------------------------------------------------------
 
 print(solution(12,[1, 5, 6, 10],[1, 2, 3, 4])) # 2
 print(solution(12,[1, 3, 4, 9, 10],[3, 5, 7])) # 1
+print(solution(12,[1, 3, 4, 9, 10],[1,3])) # 2 안나옴
+print(solution(12,[1, 3, 4, 9, 10],[1,1,1])) # 3
+print(solution(12,[1,2,5,6,9,10],[1,1,1])) # 3 안나옴
+print(solution(12,[1,2,4,5],[1,1])) # 1 안나옴
