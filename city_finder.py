42eced57e31d713551568c225559959775aa00dd
chaeminsoo <chaems97@gmail.com>
2021-09-02 15:43:28 +0900

    Incomplete

--- city_finder.py
+++ city_finder.py
@@ -3,7 +3,7 @@ from collections import deque
 n, m, k, x = map(int,input().split())
 inf = float('inf')
 roads = [[] for _ in range(n+1)]
-# result=[]
+result=False#[]
 q = deque()
 from_x_distance = [inf for _ in range(n+1)]
 from_x_distance[x] = 0
@@ -15,25 +15,32 @@ for _ in range(m):
         from_x_distance[b] = 1
         q.append(b)
 
+check = [0 for _ in range(n+1)]
+check[x] = 1
+
 cnt = 1
-#----------------
 while q:
     ref_sp = q.popleft()
-    for i in roads[ref_sp]:
-        q.append(i)
-        ref = cnt + 1
-        from_x_distance[i] = min(from_x_distance[i],ref)
-    cnt+=1
-check = 0
+    if check[ref_sp] == 1:
+        continue
+    else:
+        for i in roads[ref_sp]:
+            q.append(i)
+            ref = cnt + 1
+            from_x_distance[i] = min(from_x_distance[i],ref)
+        cnt+=1
+
+        check[ref_sp] = 1
+
 for j in range(len(from_x_distance)):
     if from_x_distance[j] == k:
-        # result.append(j)
         print(j)
-        check+=1
+        result = True
 
-if check == 0:
+if result == False:
     print(-1)
-
+#         result.append(j)
+        
 # if len(result) == 0:
 #     print(-1)
 # else:
