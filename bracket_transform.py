ce123125c4b0910444aa22e64a2790b0a84291a9
chaeminsoo <chaems97@gmail.com>
2021-09-17 19:45:10 +0900

    Incomplete

--- bracket_transform.py
+++ bracket_transform.py
@@ -1,3 +1,38 @@
+def check(p):
+    stack=[]
+    for i in p:
+        if len(stack)==0:
+            if string=='(':
+                stack.append(string)
+            else:
+                return False
+        
+        if string=='(':
+            stack.append(string)
+        elif string==')':
+            stack.pop()
+    
+    if len(stack)==0:
+        return True
+    return False
+
+def divide(p):
+    r=0
+    l=0
+    for i in q:
+        if i == '(':
+            r+=1
+        else i == ')':
+            l+=1
+        if r == l:
+            break
+    return r+l-1
+
 def solution(p):
     answer = ''
+    if check(p):
+        return p
+    else:
+        divide(p) = num
+        
     return answer
