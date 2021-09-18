03fc02d13d7980374b6b0f746ea1105f801e85a0
chaeminsoo <chaems97@gmail.com>
2021-09-18 23:26:51 +0900

    Recursive

--- bracket_transform.py
+++ bracket_transform.py
@@ -1,38 +1,59 @@
 def check(p):
     stack=[]
-    for i in p:
-        if len(stack)==0:
-            if string=='(':
-                stack.append(string)
-            else:
-                return False
-        
-        if string=='(':
-            stack.append(string)
-        elif string==')':
-            stack.pop()
     
-    if len(stack)==0:
+    for i in p:
+        if len(stack) ==0 and i ==')':
+            return False
+        
+        if i =='(':
+            stack.append(i)
+        elif i ==')':
+            stack.pop()
+    if len(stack) != 0:
+        return False
+    else:
         return True
-    return False
+    
+def change(sss):
+    result =''
+    for i in sss:
+        if i =='(':
+            result+=')'
+        elif i == ')':
+            result+='('
+    return result
 
-def divide(p):
-    r=0
-    l=0
-    for i in q:
-        if i == '(':
-            r+=1
-        else i == ')':
-            l+=1
-        if r == l:
-            break
-    return r+l-1
+def cut(string):
+    o=0
+    c=0
+    for i in range(len(string)):
+        if string[i] == '(':
+            o+=1
+        elif string[i] == ")":
+            c+=1
+        
+        if o == c:
+            return i+1
 
+def transform(string):
+    if check(string) or string == '':
+        return string
+
+    result=''
+    num = cut(string)
+    u = string[:num]
+    v = string[num:]
+    if check(u):
+        return u + transform(v)
+    else:
+        result+='(' + transform(v) + ')' + change(u[1:-1])
+        return result
+        
 def solution(p):
-    answer = ''
+    if p == '':
+        return ''
+    
     if check(p):
         return p
     else:
-        divide(p) = num
-        
-    return answer
+        return transform(p)
