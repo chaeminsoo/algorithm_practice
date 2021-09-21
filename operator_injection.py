63f4c877ec05bf605f1f1a019ac1fd699794df22
chaeminsoo <chaems97@gmail.com>
2021-09-21 23:14:18 +0900

    Incomplete

--- operator_injection.py
+++ operator_injection.py
@@ -1,34 +1,10 @@
 from collections import deque
-from itertools import permutations
+from itertools import product
 
 n = int(input())
 series = deque(map(int,input().split()))
 ops = list(map(int,input().split()))
-signs = ['+','-','*','/']
 
-operators = []
-
-for i in range(4):
-    gen = [signs[i] for _ in range(ops[i])]
-    for j in gen:
-        operators.append(j)
-
-all_case = list(permutations(operators,len(operators)))
-
-result = 0
-def check(series, case):
-    global result
-    a = series[i]
-    b = series[i+1]
-    op = case.pop()
-    ref = 
-    for i in range(len(series)):
+def calcul(series, ops):
+    result = 0
     
-        if op == '+':
-            result += (a + b)
-        elif op == '-':
-            result += (a - b)
-        elif op == '*':
-            result += (a * b)
-        elif op == '/':
-            result += (a // b)
