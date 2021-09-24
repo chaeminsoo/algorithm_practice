d90398ae5542abbc79ccc7450d0d514bf29e42d1
chaeminsoo <chaems97@gmail.com>
2021-09-24 10:23:45 +0900

    Incomplete

--- outer_wall_check.py
+++ outer_wall_check.py
@@ -1,5 +1,6 @@
 from itertools import permutations
 
+
 def close_num(target,liist):
     ref = 0
     for i in liist:
@@ -10,31 +11,31 @@ def close_num(target,liist):
 
 def solution(n, weak, dist):
     wl = len(weak)
-    new_weak = weak + [i + n for i in weak]
+    new_weak = weak + [i + n for i in weak] # 원 표현
     min_cnt = 100#float('inf')
-    all_cases = list(permutations(dist,len(dist)))
-    for sp in weak:
-        for case in all_cases:
+    all_cases = list(permutations(dist,len(dist))) # 확인할 친구 순서 모든 경우의 수
+    for sp in weak: # 시작점 바꿔가며
+        for case in all_cases: 
             wp = sp
             cnt = 0
             weak_cnt = 0
-            for friend in case:
+            for friend in case: # 시작점에서 친구 한명 출발
                 arrived = wp +friend
 
-                for i in new_weak:
+                for i in new_weak: # 한친구가 커버한 취약점 개수 확인
                     if i >= wp and i <= arrived:
                         weak_cnt +=1
-                cnt += 1
-                if weak_cnt >= wl:
-                    min_cnt = min(min_cnt,cnt)
+                cnt += 1 # 친구 한명 씀
+                if weak_cnt >= wl: # 모든 취약점을 확인한 경우
+                    min_cnt = min(min_cnt,cnt) # 최소인원 갱신
                     break
-                else:
-                    wp  = close_num(arrived,weak)
+                else: 
+                    wp  = close_num(arrived,weak) # 시작점 갱신
                     # if wp == 0:
                     #     break
                     # # print(wp)##
     
-    if min_cnt > 8:#== float('inf'):
+    if min_cnt > 8:#== float('inf'): # 모든 친구로도 커버 못했을 시
         answer = -1
     else:
         answer = min_cnt
