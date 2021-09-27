27a2ea0ab9e7362acbda1b554790a818e994b6f9
chaeminsoo <chaems97@gmail.com>
2021-09-27 14:21:58 +0900

    Incomplete

--- laboratory.py
+++ laboratory.py
@@ -4,49 +4,37 @@ n,m = map(int,input().split())
 
 field = []
 zeros = []
-viruses = []
+
+def fast_copy(list):
+    return [i[:] for i in list]
 
 for i in range(n):
     row = list(map(int,input().split()))
     for j in row:
         if j == 0:
             zeros.append([i,j])
-        if j == 2:
-            viruses.append([i,j])
     field.append(row)
 
-standard_field = field[:]
-standard_viruses = viruses[:]
+standard_field = fast_copy(field)
 
 dx = [0,0,-1,1]
-dy = [1,-1,0,0]
+dy = [-1,1,0,0]
 
-def virus_diffusion(viruses):
-    global dx, dy, field
+def virus_diffusion(x,y):
+    if field[x][y] == 2:
+        for i in range(4):  
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-    try:
-        virus = viruses.pop()    
-    except IndexError:
-        return
-
-    vr = virus[0]
-    vc = virus[1]
-    
-    for i in range(4):
-        r = vr + dy[i]
-        c = vc + dx[i]
-
-        if r >= 0 and r < n and c >= 0 and c < m:
-            if field[r][c] == 0:
-                field[r][c] = 2
-                viruses.append([r,c])
-    virus_diffusion(viruses)
+            if nx >= 0 and nx < n and ny >= 0 and ny < m:
+                if field[nx][ny] == 0:
+                    field[nx][ny] = 2
+                    virus_diffusion(nx,ny)
 
 result = 0
 new_walls = list(combinations(zeros,3))
 
-def solution():
-    global field
+def solution(field):
     cnt = 0
     for i in range(n):
         for j in range(m):
@@ -59,12 +47,14 @@ while new_walls:
 
     for k in case:
         field[k[0]][k[1]] = 1
-        
-    virus_diffusion(viruses)
-    
-    result = max(result,solution())
 
-    field = standard_field[:]
-    viruses = standard_viruses[:]
+    for i in range(n):
+        for j in range(m):
+            virus_diffusion(i,j)
+
+    result = max(result,solution(field))
+    print(result)
+
+    field = fast_copy(standard_field)
 
 print(result)
