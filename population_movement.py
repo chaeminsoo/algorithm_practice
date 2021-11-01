53760f6ccfbf3e2c4de5373460128458040966cb
chaeminsoo <chaems97@gmail.com>
2021-11-01 20:05:24 +0900

    Incomplete

--- population_movement.py
+++ population_movement.py
@@ -1,8 +1,8 @@
 n,l,r = map(int,input().split())
 
-A=[]
+nations=[]
 for _ in range(n):
-    A.append(list(map(int,input().split())))
+    nations.append(list(map(int,input().split())))
 
 visit = [[0]*n for _ in range(n)]
 
@@ -18,77 +18,38 @@ def give_not_visit(visit):
                 return [i,j]
 
 stack = []
-unions = []
 group_of_un =[]
+unions = []
 dr=[-1,1,0,0]
 dc=[0,0,-1,1]
 check = True
 
 def union(stack):
-    global visit,unions,group_of_un
     try:
-        r,c = stack.pop()
-        unions.append([r,c])
+        coordinate = stack.pop()
     except IndexError:
         return
 
+    r = coordinate[0]
+    c = coordinate[1]
+    unions.append([r,c])
     visit[r][c] = 1
-    standard = A[r][c]
-    print(standard,'8888888888')
+    
+    ref = nations[r][c]
+
     for i in range(4):
         nr = r + dr[i]
         nc = c + dc[i]
-        if nr >= 0 and nr < n and nc >= 0 and nc <n:
-            print(A[nr][nc],'555555555555')
-            print(abs(standard - A[nr][nc]),'44444444444444')
-            if abs(standard - A[nr][nc]) >= l and abs(standard - A[nr][nc]) <= r:
+
+        if nr >=0 and nr <n and nc>=0 and nc < n:
+            diff = nations[nr][nc] - ref
+            if diff >= l and diff <= r:
+                unions.append([nr,nc])
                 stack.append([nr,nc])
+
     union(stack)
 
-def move(unions):
-    global A
-    hab = 0
-    lu = len(unions)
-    if lu !=0:
-        for i in range(lu):
-            x,y = unions[i]
-            hab += A[x][y]
-        
-        val = hab//lu
+stack.append([0,0])
+union(stack)
 
-        while unions:
-            w,z = unions.pop()
-            A[w][z] = val
-    
-    return
-
-result = 0
-ref_a = []
-switch = True
-ref_a = fast_copy(A)
-while switch:
-    while True:
-        coordinate = give_not_visit(visit)
-        if coordinate == None:
-            break
-        stack.append(coordinate)
-        union(stack)
-        print(unions)
-        group_of_un.append(unions)
-        unions=[]
-    print(group_of_un)
-
-    for uns in group_of_un:
-        move(uns)
-    print(A)
-
-    if ref_a == A:
-        switch = False
-    else:
-        ref_a = fast_copy(A)
-
-    result+=1
-    visit = fast_copy(standard_v)
-    group_of_un = []
-
-print(result)
+print(unions)
