d126cda5fc3d600bfe41f8f8b4cf13f581fed108
chaeminsoo <chaems97@gmail.com>
2021-11-07 18:16:54 +0900

    DFS

--- population_movement.py
+++ population_movement.py
@@ -1,5 +1,6 @@
-n,l,r = map(int,input().split())
+n,L,R = map(int,input().split())
 
+cnt = 0
 nations=[]
 for _ in range(n):
     nations.append(list(map(int,input().split())))
@@ -17,39 +18,75 @@ def give_not_visit(visit):
             if visit[i][j] == 0:
                 return [i,j]
 
-stack = []
-group_of_un =[]
-unions = []
-dr=[-1,1,0,0]
-dc=[0,0,-1,1]
-check = True
+def check_visit(visit):
+    for i in range(n):
+        for j in range(n):
+            if visit[i][j] == 0:
+                return True
+    return False
 
-def union(stack):
+stack=[]
+
+dr = [-1,1,0,0]
+dc = [0,0,-1,1]
+
+def make_un(stack):
     try:
         coordinate = stack.pop()
     except IndexError:
         return
 
+    union.append(coordinate)
     r = coordinate[0]
     c = coordinate[1]
-    unions.append([r,c])
     visit[r][c] = 1
-    
-    ref = nations[r][c]
+
+    standard = nations[r][c]
 
     for i in range(4):
         nr = r + dr[i]
         nc = c + dc[i]
+        
+        if nr >= 0 and nr < n and nc >= 0 and nc < n:
+            ref = nations[nr][nc]
+            diff = abs(standard-ref)
 
-        if nr >=0 and nr <n and nc>=0 and nc < n:
-            diff = nations[nr][nc] - ref
-            if diff >= l and diff <= r:
-                unions.append([nr,nc])
-                stack.append([nr,nc])
+            if diff >= L and diff <= R:
+                if visit[nr][nc] != 1:
+                    stack.append([nr,nc])
 
-    union(stack)
+    make_un(stack)
+    
+while True:
+    gou = []
 
-stack.append([0,0])
-union(stack)
+    while check_visit(visit):
+        union=[]
 
-print(unions)
+        stack.append(give_not_visit(visit))
+        make_un(stack)
+
+        gou.append(union)
+
+    if len(gou) == n*n:
+        break
+
+    def move(gou):
+        for uns in gou:
+            len_uns = len(uns)
+            total = 0
+
+            for con in uns:
+                total += nations[con[0]][con[1]]
+
+            res = total // len_uns
+
+            for con in uns:
+                nations[con[0]][con[1]] = res
+
+    move(gou)
+
+    cnt +=1
+    visit = fast_copy(standard_v)
+
+print(cnt)
