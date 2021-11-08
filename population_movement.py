02ad2f56b5180909188d7fe80c81cf1ad4da871b
chaeminsoo <chaems97@gmail.com>
2021-11-08 21:32:49 +0900

    Incomplete

--- population_movement.py
+++ population_movement.py
@@ -1,5 +1,6 @@
 n,L,R = map(int,input().split())
 
+N = n*n
 cnt = 0
 nations=[]
 for _ in range(n):
@@ -18,13 +19,6 @@ def give_not_visit(visit):
             if visit[i][j] == 0:
                 return [i,j]
 
-def check_visit(visit):
-    for i in range(n):
-        for j in range(n):
-            if visit[i][j] == 0:
-                return True
-    return False
-
 stack=[]
 
 dr = [-1,1,0,0]
@@ -34,7 +28,7 @@ def make_un(stack):
     try:
         coordinate = stack.pop()
     except IndexError:
-        return
+        return 
 
     union.append(coordinate)
     r = coordinate[0]
@@ -57,32 +51,36 @@ def make_un(stack):
 
     make_un(stack)
     
+def move(gou):
+    for uns in gou:
+        len_uns = len(uns)
+        total = 0
+
+        for con in uns:
+            total += nations[con[0]][con[1]]
+
+        res = total // len_uns
+
+        for con in uns:
+            nations[con[0]][con[1]] = res
+
 while True:
     gou = []
 
-    while check_visit(visit):
+    while True:
         union=[]
+        
+        coordin = give_not_visit(visit)
+        if coordin == None:
+            break
+        else:
+            stack.append(coordin)
+            make_un(stack)
 
-        stack.append(give_not_visit(visit))
-        make_un(stack)
+            gou.append(union)
 
-        gou.append(union)
-
-    if len(gou) == n*n:
+    if len(gou) == N:
         break
-
-    def move(gou):
-        for uns in gou:
-            len_uns = len(uns)
-            total = 0
-
-            for con in uns:
-                total += nations[con[0]][con[1]]
-
-            res = total // len_uns
-
-            for con in uns:
-                nations[con[0]][con[1]] = res
 
     move(gou)
 
@@ -90,3 +88,15 @@ while True:
     visit = fast_copy(standard_v)
 
 print(cnt)
+
+# def a():
+#     n = random.randint(1,5)
+#     l = random.randint(1,100)
+#     r = random.randint(1,100)
+#     list = [[] for _ in range(n)]
+#     for i in range(n):
+#             for j in range(n):
+#                     list[i].append(random.randint(0,100))
+#     print(n,l,r)
+#     for k in range(n):
+#             print(list[k])
