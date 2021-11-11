9d004726b8f46e74cd4bb1768c613d85d1b6edb3
chaeminsoo <chaems97@gmail.com>
2021-11-11 22:03:54 +0900

    Incomplete

--- population_movement.py
+++ population_movement.py
@@ -1,3 +1,4 @@
+# def sol(n,L,R,nations):
 n,L,R = map(int,input().split())
 
 N = n*n
@@ -31,8 +32,7 @@ def make_un(stack):
         return 
 
     union.append(coordinate)
-    r = coordinate[0]
-    c = coordinate[1]
+    r,c = coordinate
     visit[r][c] = 1
 
     standard = nations[r][c]
@@ -88,15 +88,98 @@ while True:
     visit = fast_copy(standard_v)
 
 print(cnt)
-
+# return cnt
+# ******************************************************************
 # def a():
-#     n = random.randint(1,5)
+#     n = random.randint(1,50)
 #     l = random.randint(1,100)
 #     r = random.randint(1,100)
 #     list = [[] for _ in range(n)]
 #     for i in range(n):
 #             for j in range(n):
 #                     list[i].append(random.randint(0,100))
-#     print(n,l,r)
-#     for k in range(n):
-#             print(list[k])
+#     return n,min(l,r),max(l,r),list
+# #************************************************************************
+
+# def sol2(n,l,r,graph):
+#     # n, l, r = map(int, input().split(' '))
+#     # graph = [list(map(int, input().split(' '))) for _ in range(n)]
+
+#     d_x = [-1, 0, 1, 0]
+#     d_y = [0, 1, 0, -1]
+
+#     is_move = False
+
+
+#     def bfs(c_x, c_y, visited, grpah):
+#         global is_move
+#         people = graph[c_x][c_y]
+#         count = 1
+#         queue = deque()
+#         queue.append((c_x, c_y))
+#         visited[c_x][c_y] = True
+#         temp = list()
+#         temp.append((c_x, c_y))
+
+#         while queue:
+#             pop_x, pop_y = queue.popleft()
+
+#             for i in range(4):
+#                 n_x = pop_x + d_x[i]
+#                 n_y = pop_y + d_y[i]
+
+#                 if n_x < 0 or n_x >= n or n_y < 0 or n_y >= n:
+#                     continue
+
+#                 if visited[n_x][n_y]:
+#                     continue
+
+#                 if l <= abs(grpah[pop_x][pop_y] - grpah[n_x][n_y]) <= r:
+#                     visited[n_x][n_y] = True
+#                     queue.append((n_x, n_y))
+#                     people += graph[n_x][n_y]
+#                     count += 1
+#                     temp.append((n_x, n_y))
+
+#         move_people = people // count
+
+#         if count > 1:
+#             is_move = True
+#             for x, y in temp:
+#                 graph[x][y] = move_people
+
+#     answer = 0
+
+#     while True:
+#         is_move = False
+#         visited = [[False] * n for _ in range(n)]
+#         for i in range(n):
+#             for j in range(n):
+#                 if not visited[i][j]:
+#                     bfs(i, j, visited, graph)
+
+#         if is_move:
+#             answer += 1
+#         else:
+#             break
+
+#     # print(answer)
+#     return answer
+# #************************************************
+# # while True:
+# #     n,l,r,lis = a()
+# #     v1 = sol(n,l,r,lis)
+# #     v2 = sol2(n,l,r,lis)
+
+# #     if v1 != v2:
+# #         print(v1,v2,'\n')
+# #         print(n,l,r,'\n')
+# #         for i in range(len(lis)):
+# #             print(lis[i])
+# #         print('\n')
+# #         print(v1)
+# #         break
+# #***************************
+
+# print(sol(2,46,73,[[32,32],[32,71]]))
+# print(sol2(2,46,73,[[32,32],[32,71]]))
