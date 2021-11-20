52b15efb88e00f7a6bbd16fa20f28581d3cd219e
chaeminsoo <chaems97@gmail.com>
2021-11-20 22:37:20 +0900

    Incomplete

--- move_on_the_block.py
+++ move_on_the_block.py
@@ -1,4 +1,20 @@
+from collections import deque
+
 def solution(board):
     answer = 0
     
+    shortest_distance = [i[:] for i in board]
+
+    drone = {(0,0),(0,1)}
+    dr = [-1,1,0,0]
+    dc = [0,0,-1,1]
+
+    def drone_move(drone):
+        q = deque()
+        q.append(drone)
+
+        
+
+    
+
     return answer
