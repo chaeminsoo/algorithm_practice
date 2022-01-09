51109585b3ad633960de150f6d9213dd54845f9b
chaeminsoo <chaems97@gmail.com>
2022-01-09 16:14:13 +0900

    Sort

--- fail_rate.py
+++ fail_rate.py
@@ -1,30 +1,22 @@
 def solution(N, stages):
     answer = []
-    num_players = len(stages)
-    max_num_players = num_players-1
+    np = len(stages)
     fail_rate=[]
-    stages.sort()
+    stage_count = [0 for _ in range(N+1)]
+    
+    for stage in stages:
+        stage_count[stage-1] +=1
+    
+    for i in range(len(stage_count)-1):
+        if np == 0:
+            fail_rate.append((i+1,0))
+        else:
+            players = stage_count[i]
+            fail_rate.append((i+1,players/np))
+            np-=players
 
-    cnt = 0
-    index=0
-    for i in range(1,N+1):
-        while index < max_num_players:
-            if i == stages[index]:
-                cnt+=1
-                index+=1
-                continue
-            else:
-                break
-        fail_rate.append((i,cnt/num_players))
-        num_players-=cnt
-        cnt = 0
-        
-    fail_rate.sort(key= lambda x: (-x[1],x[0]))
-
-    for i in fail_rate:
-        answer.append(i[0])
+    fail_rate.sort(key= lambda x:(-x[1],x[0]))
+    for j in fail_rate:
+        answer.append(j[0])
 
     return answer
-
-print(solution(4,[4,4,4,4,4]))
-print(solution(5,[2, 1, 2, 6, 2, 4, 3, 3]))
