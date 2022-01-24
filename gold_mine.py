749b678371d3812cc13f1c3c5bf03d94b1162f7e
chaeminsoo <chaems97@gmail.com>
2022-01-25 07:48:35 +0900

    Dinamic_Programming

--- gold_mine.py
+++ gold_mine.py
@@ -1,5 +1,4 @@
 t = int(input())
-n,m = map(int,input().split())
 nm = []
 gold = []
 
@@ -7,34 +6,32 @@ for _ in range(t):
     nm.append(tuple(map(int,input().split())))
     gold.append(list(map(int,input().split())))
 
-
-def moving(gold,coordinate,n,m,ref):
-    r,c = coordinate
-    ref += gold[r][c]
-    if c+1 >= m:
-        return ref
-    for i in (-1,0,1):
-        if r+i >=0 and r+i <=n:
-            moving(gold,coordinate,r+i,c+1,ref)
+def collect(r,c,gold_field,n):
+    ref = gold_field[r][c]
+    answer=[]
+    if r-1 >= 0:
+        answer.append(ref + gold_field[r-1][c-1])
+    if r+1 < n:
+        answer.append(ref + gold_field[r+1][c-1])
+    answer.append(ref + gold_field[r][c-1])
+    return max(answer)    
 
 def solution(nm,gold):
     n,m = nm
-    answer=[]
+    answer=0
     gold_field = []
 
     for i in range(0,n):
         gold_field.append(gold[m*i:m*(i+1)])
-        
-    cnt=1
-    for i in gold:
-        gold_field[cnt//m].append(i)
-        cnt+=1
 
-    for k in range(n):
-        ref = 0
-        answer.append(moving(gold_field,(k,0),n,m,ref))
+    for k in range(1,m):
+        for j in range(n):
+            gold_field[j][k] = collect(j,k,gold_field,n)
+    
+    for l in range(n):
+        answer = max(answer,gold_field[l][m-1])
 
-    return max(answer)
+    return answer
 
 for i in range(t):
     print(solution(nm[i],gold[i]))
