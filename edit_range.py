356b950b388e6b2d20819318b78ef1b5bb37e243
chaeminsoo <chaems97@gmail.com>
2022-01-29 23:51:53 +0900

    DP_LCS

---
+++ edit_range.py
@@ -0,0 +1,13 @@
+a = input()
+b = input()
+
+dp = [[0]*(len(b)+1) for _ in range(len(a)+1)]
+
+for i in range(len(a)-1,-1,-1):
+    for j in range(len(b)-1,-1,-1):
+        if a[i] == b[j]:
+            dp[i][j] = dp[i+1][j+1]+1
+        else:
+            dp[i][j] = max(dp[i+1][j],dp[i][j+1])
+
+print(len(b)-dp[0][0])
