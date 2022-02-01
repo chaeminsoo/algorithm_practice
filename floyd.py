077186a018dde9c792de5146c5ad44d15349834f
chaeminsoo <chaems97@gmail.com>
2022-02-01 15:35:55 +0900

    Floyd

--- floyd.py
+++ floyd.py
@@ -2,6 +2,7 @@ n = int(input())
 m = int(input())
 inf = 1e9
 city =[[inf]*n for _ in range(n)]
+
 for i in range(n):
     for j in range(n):
         if i==j:
@@ -9,23 +10,18 @@ for i in range(n):
 
 for _ in range(m):
     a,b,c = map(int,input().split())
-    city[a-1][b-1] = c
+    if c < city[a-1][b-1]:
+        city[a-1][b-1] = c
 
 for i in range(n):
     for a in range(n):
-        if a == i:
-            break
         for b in range(n):
-            if b == i:
-                break
             city[a][b] = min(city[a][b],city[a][i]+city[i][b])
 
 for i in city:
-    while True:
-        try:
-            i[i.index(inf)] = 0
-        except ValueError:
-            break
-
-for i in city:
-    print(i)
+    for j in i:
+        if j == inf:
+            print(0,end=' ')
+        else:
+            print(j,end=' ')
+    print()
