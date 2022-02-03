5583d407176189dcacfe421fff01bf40e3151b8a
chaeminsoo <chaems97@gmail.com>
2022-02-03 22:35:33 +0900

    Dijkstra

--- mars_explore.py
+++ mars_explore.py
@@ -1,29 +1,40 @@
-t = int(input())
+import heapq
 
-def solution(n,field):
-    dr = [-1,1,0,0]
-    dc = [0,0,-1,1]
-    for i in range(n):
-        for j in range(n):
-            ref = []
-            if i-1 >= 0:
-                ref.append(field[i][j]+field[i-1][j])
-            if j-1 >= 0:
-                ref.append(field[i][j]+field[i][j-1])
-            try:
-                field[i][j] = min(ref)
-            except ValueError:
-                pass
-    return field#[n-1][n-1]
-# answer=[]
+t = int(input())
+INF = 1e9
+dr = [-1,1,0,0]
+dc = [0,0,-1,1]
+
+def solution(n,graph):
+    distance = [[INF]*n for _ in range(n)]
+    visited = [[False]*n for _ in range(n)]
+    distance[0][0] = graph[0][0]
+    visited[0][0] = True
+    q=[]
+    heapq.heappush(q,[graph[0][0],0,0])
+    while q:
+        v,r,c = heapq.heappop(q)
+        visited[r][c] = True
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+
+            if nr >= 0 and nc >= 0 and nr < n and nc < n:
+                if visited[nr][nc] == False:
+                    new_dis = graph[nr][nc] + v
+                    if new_dis < distance[nr][nc]:
+                        distance[nr][nc] = new_dis
+                        heapq.heappush(q,[new_dis,nr,nc])
+
+    return distance[n-1][n-1]
+
+answer=[]
 for _ in range(t):
     n = int(input())
     field = []
     for _ in range(n):
         field.append(list(map(int,input().split())))
+    answer.append(solution(n,field))
 
-    for i in solution(n,field):
-        print(i)
-#     answer.append(solution(n,field))
-# for i in answer:
-#     print(i)
+for i in answer:
+    print(i)
