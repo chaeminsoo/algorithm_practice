c0c761a48b4cef3f7c35d4c2656b7d1148f32ae9
chaeminsoo <chaems97@gmail.com>
2022-02-11 18:14:47 +0900

    Incomplete

---
+++ final_rank.py
@@ -0,0 +1,16 @@
+# 3665
+t = int(input())
+
+def solution(n,prev_rank,m,changeed):
+
+    return
+
+for _ in range(t):
+    n = int(input())
+    prev_rank=list(map(int,input().split()))
+    m = int(input())
+    changeed = []
+    for _ in range(m):
+        a,b = map(int,input().split())
+        changeed.append((a,b))
+    print(solution(n,prev_rank,m,changeed))
