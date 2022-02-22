8a46359a77ed68a6a451be44ff4b6e6fb1dc5de9
chaeminsoo <chaems97@gmail.com>
2022-02-22 23:58:43 +0900

    Implemetaion

--- adult_shark.py
+++ adult_shark.py
@@ -1,7 +1,6 @@
 n, m, k = map(int,input().split())
 shark_where = [0]*(m+1)
 shark_direction = [0]*(m+1)
-shark_cnt = m
 
 for i in range(n):
     data = list(map(int,input().split()))
@@ -25,6 +24,13 @@ smell_field = [[0]*n for _ in range(n)]
 dr = [-1,1,0,0]
 dc = [0,0,-1,1]
 
+def shark_cnt(shark_where):
+    cnt = 0
+    for i in shark_where:
+        if i != 0:
+            cnt += 1
+    return cnt
+
 def spray_smell(shark_where):
     for i in range(1,m+1):
         if shark_where[i] == 0:
@@ -32,37 +38,50 @@ def spray_smell(shark_where):
         r,c = shark_where[i]
         smell_field[r][c] = [i,k]
 
-def shark_check(r,c,i,shark_cnt):
+def shark_check(r,c,shark_num):
     if shark_field[r][c] != 0:
         ref = shark_field[r][c]
-        if ref < i:
-            shark_where[i] = 0
-            shark_cnt -= 1
+        if ref < shark_num:
+            shark_where[shark_num] = 0
         else:
-            shark_field[r][c] = i
+            shark_field[r][c] = shark_num
+            shark_where[shark_num] = [r,c]
             shark_where[ref] = 0
-            shark_cnt -= 1
     else:
-        shark_field[r][c] = i
+        shark_field[r][c] = shark_num
+        shark_where[shark_num] = [r,c]
         
-def shark_move(shark_where,shark_cnt):
+def shark_move(shark_where):
     for i in range(1,m+1):
         if shark_where[i] == 0:
             continue
         r,c = shark_where[i]
         now_direct = shark_direction[i]
-        nxt_direct = shark_prefer[i][now_direct]
-        for nxt in nxt_direct:
+        nxt_directs = shark_prefer[i][now_direct]
+        ref = True
+        for nxt in nxt_directs:
             nr = r + dr[nxt-1]
             nc = c + dc[nxt-1]
             if nr >=0 and nr < n and nc >=0 and nc <n:
                 if smell_field[nr][nc] ==0:
-                    shark_check(nr,nc,i,shark_cnt)
+                    shark_check(nr,nc,i)
+                    shark_field[r][c] = 0
+                    shark_direction[i] = nxt
+                    ref = False
+                    break
                 else:
-                    if smell_field[nr][nc][0] != i:
-                        continue
-                    else:
-                        shark_check(nr,nc,i,shark_cnt)
+                    continue
+        
+        if ref == True:
+            for nxt in nxt_directs:
+                nr = r + dr[nxt-1]
+                nc = c + dc[nxt-1]
+                if nr >=0 and nr < n and nc >=0 and nc <n:
+                    if smell_field[nr][nc][0] == i:
+                        shark_check(nr,nc,i)
+                        shark_field[r][c] = 0
+                        shark_direction[i] = nxt
+                        break
 
 def remove_smell():
     for i in range(n):
@@ -70,22 +89,23 @@ def remove_smell():
             if smell_field[i][j] == 0:
                 continue
             else:
+                if shark_field[i][j] !=0:
+                    continue
                 smell_field[i][j][1] -= 1
                 if smell_field[i][j][1] == 0:
                     smell_field[i][j] = 0
-
+                    
 time_cnt = 0
 spray_smell(shark_where)
 while time_cnt < 1000:
-    remove_smell()
-    shark_move(shark_where,m)
+    shark_move(shark_where)
     spray_smell(shark_where)
+    remove_smell()
     time_cnt += 1
-    if shark_cnt == 1:
+    if shark_cnt(shark_where) == 1:
         break
 
-if shark_cnt != 1:
-    print(shark_cnt)
+if shark_cnt(shark_where) != 1:
     print(-1)
 else:
     print(time_cnt)
