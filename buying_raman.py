8421180078ae492ebdeb5b20f5e8eada96b65b75
chaeminsoo <chaems97@gmail.com>
2022-02-26 23:23:47 +0900

    Incomplete

--- buying_raman.py
+++ buying_raman.py
@@ -4,26 +4,34 @@ factories = list(map(int,input().split()))
 cursor_ = 0
 cost = 0
 
-while cursor_<n:
-    ref = 0
+while cursor_ < n:
     if factories[cursor_] == 0:
-        cursor_ += 1
+        cursor_ +=1
+        continue
+    ref = cursor_
+    while factories[ref] != 0:
+        ref += 1
+        if ref == cursor_ + 2:
+            break
 
-    for _ in range(3):
-        try:
-            if factories[cursor_+ref] != 0:
-                ref += 1
-        except IndexError:
-            break
+    if ref == cursor_:
+        cost += 3*factories[cursor_]
+        factories[cursor_] = 0
+    elif ref == cursor_+1:
+        stand = min(factories[cursor_],factories[cursor_+1])
+        cost += 5*stand
+        for i in range(2):
+            factories[cursor_+i] -= stand
+    elif ref == cursor_+2:
+        if factories[cursor_+1] > factories[cursor_+2]:
+            stand = min(factories[cursor_],factories[cursor_+1])
+            cost += 5*stand
+            for i in range(2):
+                factories[cursor_+i] -= stand
         else:
-            break
-    
-    lowest = min(factories[cursor_:cursor_+ref])
-    
-    if ref ==1:
-        cost += lowest*3
-        factories[cursor_+ref-1] -= 1
-    elif ref == 2:
-        cost += lowest*5
-    elif ref == 3:
-        cost += lowest*7
+            stand = min(factories[cursor_],factories[cursor_+1],factories[cursor_+2])
+            cost += 7*stand
+            for i in range(3):
+                factories[cursor_+i] -= stand
+
+print(cost)
