0b388fad726aa753b4ccea0f2b7b4cdb4300e68c
chaeminsoo <chaems97@gmail.com>
2022-02-27 16:18:33 +0900

    Greedy

--- buying_raman.py
+++ buying_raman.py
@@ -2,36 +2,41 @@ n = int(input())
 factories = list(map(int,input().split()))
 
 cursor_ = 0
-cost = 0
+answer = 0
+
+for _ in range(2):
+    factories.append(0)
 
 while cursor_ < n:
-    if factories[cursor_] == 0:
-        cursor_ +=1
+    i1, i2, i3 = factories[cursor_], factories[cursor_+1], factories[cursor_+2]
+
+    if i1 == 0:
+        cursor_ += 1
         continue
-    ref = cursor_
-    while factories[ref] != 0:
-        ref += 1
-        if ref == cursor_ + 2:
-            break
+    
+    if i2 == 0:
+        answer += 3*i1
+        factories[cursor_] -= i1
+        continue
 
-    if ref == cursor_:
-        cost += 3*factories[cursor_]
-        factories[cursor_] = 0
-    elif ref == cursor_+1:
-        stand = min(factories[cursor_],factories[cursor_+1])
-        cost += 5*stand
-        for i in range(2):
-            factories[cursor_+i] -= stand
-    elif ref == cursor_+2:
-        if factories[cursor_+1] > factories[cursor_+2]:
-            stand = min(factories[cursor_],factories[cursor_+1])
-            cost += 5*stand
-            for i in range(2):
-                factories[cursor_+i] -= stand
-        else:
-            stand = min(factories[cursor_],factories[cursor_+1],factories[cursor_+2])
-            cost += 7*stand
-            for i in range(3):
-                factories[cursor_+i] -= stand
+    if i3 == 0:
+        ref = min(i1,i2)
+        answer += 5*ref
+        factories[cursor_] -= ref
+        factories[cursor_+1] -= ref
+        continue
 
-print(cost)
+    if i2 > i3:
+        ref = min(i1, i2 - i3)
+        answer += 5*ref
+        factories[cursor_] -= ref
+        factories[cursor_+1] -= ref
+        continue
+
+    ref = min(i1,i2,i3)
+    answer += 7*ref
+    factories[cursor_] -= ref
+    factories[cursor_+1] -= ref
+    factories[cursor_+2] -= ref
+
+print(answer)
