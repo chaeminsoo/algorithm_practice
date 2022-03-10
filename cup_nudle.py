8d9a290cc23c35d722baa980a6e709a4130461e5
chaeminsoo <chaems97@gmail.com>
2022-03-10 22:58:37 +0900

    Greedy

--- cup_nudle.py
+++ cup_nudle.py
@@ -1,16 +1,19 @@
 #1781
+import heapq
+
 n = int(input())
 data = []
 for _ in range(n):
     d,p = map(int,input().split())
     data.append((d,p))
 
-data.sort(key= lambda x: (-x[1],x[0]))
-ref_time = 0
-ans = 0
+data.sort()
+heap = []
 for i in data:
     deadline, nudle = i
-    if ref_time < deadline:
-        ans += nudle
-        ref_time += 1
-print(ans)
+    
+    heapq.heappush(heap,nudle)
+    if deadline < len(heap):
+        heapq.heappop(heap)
+    print(heap)
+print(sum(heap))
