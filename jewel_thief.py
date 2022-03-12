7e227a25446f8db7100f21592fb58432d1c26f9b
chaeminsoo <chaems97@gmail.com>
2022-03-12 20:53:22 +0900

    Incomplete

--- jewel_thief.py
+++ jewel_thief.py
@@ -1,9 +1,19 @@
 # 1202
-from bisect import bisect_left
 import sys
 
 input = sys.stdin.readline
 n,k = map(int,input().split())
+
+def bi_search(target,s,e,list_):
+    mid = (s+e)//2
+    if mid == s:
+        return mid
+    if list_[mid] == target:
+        return mid
+    elif list_[mid] < target:
+        return bi_search(target,mid,e,list_)
+    elif list_[mid] > target:
+        return bi_search(target,s,mid,list_)
 
 jewel = []
 bag = []
@@ -17,17 +27,25 @@ for _ in range(k):
 jewel.sort(key= lambda x:(-x[1],x[0]))
 bag.sort()
 ans = 0
-for i in jewel:
-    weight, val = i
-    idx = bisect_left(bag,weight)
-    if idx == k:
-        idx-=1
-    if k <= 0:
-        break
+
+for weight, val in jewel:
+    if k==0: break
+    ref_idx = bi_search(weight,0,k-1,bag)
     
-    if bag[idx] < weight:
-        continue
-    else:
-        ans+=val
-        k-=1
+    if bag[ref_idx] == weight:
+        bag.pop(ref_idx)
+        ans += val
+        k -= 1
+    elif bag[ref_idx] < weight:
+        try:
+            bag.pop(ref_idx+1)
+            ans += val
+            k -= 1
+        except IndexError:
+            continue
+    elif bag[ref_idx] > weight:
+        bag.pop(ref_idx)
+        ans += val
+        k -= 1
+
 print(ans)
