534aa4049e4aa2938f366e0d626aa43407d60e9b
chaeminsoo <chaems97@gmail.com>
2022-04-02 00:02:22 +0900

    Greedy

--- filling_fuel.py
+++ filling_fuel.py
@@ -17,12 +17,11 @@ cnt = 0
 
 while True:
     for i in gas_station[bisect_left(gas_station,(pre_cursor_,101)):bisect_left(gas_station,(now_cursor_,101))]:
-        heapq.heappush(heap,i[1])
-    
+        heapq.heappush(heap,(-i[1],i[1]))
     if now_cursor_ < needed_gas:
         pre_cursor_ = now_cursor_
         try:
-            now_cursor_ += heapq.heappop(heap)
+            now_cursor_ += heapq.heappop(heap)[1]
             cnt += 1
         except IndexError:
             cnt = -1
