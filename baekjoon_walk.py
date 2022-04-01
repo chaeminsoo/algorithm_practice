ed4167402f574021a42df3f1202b46a805ec50c4
chaeminsoo <chaems97@gmail.com>
2022-04-01 17:16:12 +0900

    Greedy

--- baekjoon_walk.py
+++ baekjoon_walk.py
@@ -1,20 +1,31 @@
 # 1459
 x,y,w,s = map(int,input().split())
 
-diag_ = True
+diag_1 = True
+diag_2 = False
 if w*2 <= s:
-    diag_ = False
+    diag_1 = False
+if w*2 >= s*2:
+    diag_2 = True
 
-if diag_:
+if diag_1:
     ans = 0
     if x-y>=0:
         ans += s*y
         x-=y
-        if x%2 == 0:
-            ans += s*x
+        if diag_2:
+            ans += ((x//2)*2)*s
+            ans += (x%2)*w
         else:
-            ans += s*(x-1)
-            ans += w
+            ans += x*w
+    elif x-y<0:
+        ans += s*x
+        y-=x
+        if diag_2:
+            ans += ((y//2)*2)*s
+            ans += (y%2)*w
+        else:
+            ans += y*w
     print(ans)
 else:
     print((x+y)*w)
