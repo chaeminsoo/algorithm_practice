9a452c48ff0ef4d30a8de18c7d119bbf9b497ec2
chaeminsoo <chaems97@gmail.com>
2022-04-03 17:48:25 +0900

    Incomplete

--- baekjoon_race.py
+++ baekjoon_race.py
@@ -1,22 +1,36 @@
 #1508
-import heapq
+from bisect import bisect_left
 
 n,m,k = map(int,input().split())
 ref_location = list(map(int,input().split()))
 
-interval_list = []
-for i in range(0,k-1):
-    heapq.heappush(interval_list,(ref_location[i+1] - ref_location[i],i))
+def bi_search(list_,interval_):
+    cnt = 1
+    st = list_[0]
+    ed = list_[-1]
+    ref = st
+    while ref < ed:
+        ref += interval_
+        if cnt >= m:
+            break
+        if bisect_left(list_,ref) < k:
+            cnt += 1
+            continue
+    if cnt >= m:
+        return True
+    else:
+        return False
 
-for _ in range(k-m):
-    heapq.heappop(interval_list)
+start_interval = 0
+end_interval = n
+mid_interval = (start_interval+end_interval)//2
 
-print(interval_list)
-# ans = ['0']*k
-# while interval_list:
-#     # ref = interval_list.pop()[1]
-#     ref = heapq.heappop(interval_list)[1]
-#     ans[ref] = '1'
-#     ans[ref+1] = '1'
+while True:
+    if mid_interval == start_interval or mid_interval == end_interval:
+        break
+    if bi_search(ref_location,mid_interval) == True:
+        mid_interval = (mid_interval+end_interval)//2
+    elif bi_search(ref_location,mid_interval) == False:
+        mid_interval = (mid_interval+start_interval)//2
 
-# print(''.join(ans))
+print(mid_interval)
