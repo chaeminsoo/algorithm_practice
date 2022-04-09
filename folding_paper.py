e7cbdcf45288e3b91c6f1864594bf5f5391d7ee4
chaeminsoo <chaems97@gmail.com>
2022-04-09 18:22:43 +0900

    Greedy

--- folding_paper.py
+++ folding_paper.py
@@ -1,23 +1,17 @@
 #1802
-t = int(input())
-
-cases = []
-for _ in range(t):
-    a = input()
-    cases.append(a)
-
 def solution(target):
+    if len(target) == 1:
+        return 'YES'
     stnd = int((len(target)-1)/2)
     ref_1 = target[:stnd]
     ref_2 = target[stnd+1:][::-1]
-    ans = 'YES'
     for j in range(len(ref_1)):
         if int(ref_1[j]) + int(ref_2[j]) == 1:
             continue
         else:
-            ans = 'NO'
-            break
-    return ans
+            return 'NO'
+    return solution(ref_1)
 
-for i in cases:
-    print(solution(i))
+
+for tc in range(int(input())):
+    print(solution(input()))
