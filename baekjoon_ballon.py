ca78b6fc3788fec795509483129eabf7d59eaafb
chaeminsoo <chaems97@gmail.com>
2022-04-13 21:05:02 +0900

    Incomplete

--- baekjoon_ballon.py
+++ baekjoon_ballon.py
@@ -7,6 +7,27 @@ while True:
     ans = 0
     for _ in range(n):
         k_,da_,db_ = map(int,input().split())
-        teams.append((k_,da_,db_))
+        difference = abs(da_ - db_)
+        teams.append((difference,k_,da_,db_))
 
-    
+    teams.sort()
+    ans = 0
+    for i in teams:
+        dif,k,da,db = i
+        if da >= db:
+            if k <= b:
+                b -= k
+                ans += k*db
+            else:
+                ref = k - b
+                ans += b*db
+                ans += ref*da
+        elif da < db:
+            if k <= a:
+                a -= k
+                ans += k*da
+            else:
+                ref = k - a
+                ans += a*da
+                ans += ref*db
+    print(ans)
