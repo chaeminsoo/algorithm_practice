3c26f73720a738c65d088c07f77b3bc1c67048a9
chaeminsoo <chaems97@gmail.com>
2022-04-15 14:19:23 +0900

    Incomplete

--- marble_escape.py
+++ marble_escape.py
@@ -1,8 +1,24 @@
+#13469
+from collections import deque
+
 n,m = map(int,input().split())
 board = []
-for _ in range(n):
+status_ = [0,0,0]
+for i in range(n):
     data = list(input())
+    if 'R' in data:
+        status_[0] = [i,data.index('R')]
+    if 'B' in data:
+        status_[1] = [i,data.index('B')]
     board.append(data)
 
 def go():
     return
+
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+q = deque()
+q.append(status_)
+
+while True:
+    
