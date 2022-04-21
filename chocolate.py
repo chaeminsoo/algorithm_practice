587af47eb751f0ed3713a2cf66b94eb922c75e34
chaeminsoo <chaems97@gmail.com>
2022-04-22 00:15:21 +0900

    Incomplete

--- chocolate.py
+++ chocolate.py
@@ -5,5 +5,17 @@ def find_n(kk):
     a = 0
     while 2**a <kk:
         a+=1
-    return 2**a
+    return 2**a,a
 
+n,a = find_n(k)
+b_k = format(k,'b')
+lenbn = len(b_k)
+print('==',b_k)
+ans = a
+for i in range(lenbn-1,-1,-1):
+    check_ = b_k[i]
+    if check_ == '1':
+        break
+    else:
+        ans-=1
+print(n,ans)
