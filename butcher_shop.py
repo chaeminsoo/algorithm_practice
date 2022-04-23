314c918ac780b2d691430472641031028c6c5914
chaeminsoo <chaems97@gmail.com>
2022-04-23 21:46:14 +0900

    Incomplete

---
+++ butcher_shop.py
@@ -0,0 +1,34 @@
+#2258
+n,m = map(int,input().split())
+meat = []
+total = 0
+for _ in range(n):
+    w,c = map(int,input().split())
+    total += w
+    meat.append([w,c])
+
+meat.sort(key=lambda x: -x[1])
+ans = 0
+
+pre_cost = -1
+
+for i in meat:
+    weight, cost = i
+    if pre_cost != cost:
+        pre_cost = cost
+        total += weight
+        ans = cost
+        if total >= m:
+            break
+        else:
+            continue
+    else:
+        total += weight
+        ans += cost
+        if total >= m:
+            break
+        else:
+            continue
+
+print(meat)
+print(ans)
