f3e8fb50088e3204eec69e44dfa9e6a3b8381d7b
chaeminsoo <chaems97@gmail.com>
2022-04-23 12:12:37 +0900

    Greedy_LineSweep

--- bus_route.py
+++ bus_route.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 n = int(input())
 m = int(input())
 routes = []
-ans = []
+ans = [True]*m
 for i in range(m):
     a,b = map(int,input().split())
     if a > b:
@@ -13,19 +13,19 @@ for i in range(m):
     else:
         routes.append([a+n,b+n,i+1])
     routes.append([a,b,i+1])
-    ans.append(i+1)
 
 routes.sort(key=lambda x:(x[0],-x[1]))
 
 very_right = 0
-pre_st = -1
 
 for route in routes:
     st,ed,num = route
     if ed > very_right:
         very_right = ed
     else:
-        ans.remove(num)
+        ans[num-1] = False
 
-for an in ans:
-    print(an,end=' ')
+for an in enumerate(ans):
+    idx,tf = an
+    if tf:
+        print(idx+1,end=' ')
