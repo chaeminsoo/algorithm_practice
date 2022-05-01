5ef5e8551fd46e1d0cd0db605c242060f6451c16
chaeminsoo <chaems97@gmail.com>
2022-05-01 23:08:19 +0900

    Greedy

--- make_it_big.py
+++ make_it_big.py
@@ -1,18 +1,22 @@
+#2812
 n,k = map(int,input().split())
-ref = input()
-want_digit = n-k
-ans = ''
-cursor1 = 0
-cursor2 = -want_digit
-while cursor2 < 0:
-    check_list = list(map(int,ref[cursor1:cursor2]))
-    max_num = max(check_list)
-    if max_num > int(ref[cursor2]):
-        ans += str(max_num)
-        idx = check_list.index(max_num)
-        cursor1 = idx+1
-        cursor2 +=1
+ref = list(map(int,input()))
+ans = []
+cnt = 0
+for i in ref:
+    if not ans:
+        ans.append(i)
     else:
-        ans+=ref[cursor2:]
-        break
-print(int(ans))
+        while ans:
+            if cnt >= k:
+                break
+            top = ans.pop()
+            if top >= i:
+                ans.append(top)
+                break
+            else:
+                cnt+=1
+                continue
+        ans.append(i)
+for i in ans[:n-k]:
+    print(i,end='')
